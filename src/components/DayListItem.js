6d7dacdd5fc164dc645e5e1681cb3ab28b0e2b48
Carter <carter.hemington@gmail.com>
2022-02-22 16:46:23 -0500

    added formatSpots function to display proper formats for 0 or 1 spot remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,11 +5,23 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
 const dayClass = classNames("day-list__item", { "day-list__item--selected": props.selected,  "day-list__item--full":  !props.spots });
+function formatSpots(props) {
+let spotFormat = [];
+  if(props.spots > 1)  {
+    spotFormat = `${props.spots} spots remaining`;
+  } else if (props.spots === 1) {
+    spotFormat = `${props.spots} spot remaining`
+  } else if (props.spots === 0) {
+    spotFormat = `no spots remaining`
+  }
+  return spotFormat;
+}
+
 
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
   );
 }
