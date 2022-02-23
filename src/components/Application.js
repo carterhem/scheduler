bdbf6bd29e92b70f6f2949fda7fe82b28c19a0c1
Carter <carter.hemington@gmail.com>
2022-02-23 09:35:06 -0500

    trying to get state to show

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,6 @@ import DayList from "./DayList";
 import { useState } from "react";
 import "components/Application.scss";
 
-
 const days = [
   {
     id: 1,
@@ -24,32 +23,32 @@ const days = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  console.log(day);
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu"><DayList
- days={days}
-  day={day}
-   setDay={setDay} 
-   />
-   </nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList 
+          days={days} 
+          day={day} 
+          setDay={setDay} 
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
-
-    
   );
 }
