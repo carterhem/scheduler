83086d6f2014a044d521dc76e90d02c1b920b817
Carter <carter.hemington@gmail.com>
2022-02-22 19:42:31 -0500

    added state and passed day and days to daylist

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DayList from "./DayList";
+import { useState } from "react";
 import "components/Application.scss";
 
 
@@ -22,6 +23,7 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -33,8 +35,8 @@ export default function Application(props) {
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu"><DayList
  days={days}
-  day={"Monday"}
-   setDay={day => console.log(day)} 
+  day={day}
+   setDay={setDay} 
    />
    </nav>
 <img
