3d4bc8cd4ee750d5e1e750ebf490c9a20200f766
Carter <carter.hemington@gmail.com>
2022-02-23 15:02:57 -0500

    interviewlist and item created and refactored

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,25 +5,16 @@ import classNames from "classnames";
 export default function InterviewerListItem(props) {
 const interviewerClass = classNames("interviewers__item", {"interviewers__item--selected": props.selected})
 
-  if(!props.selected) {
-   return <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
+ return (
+  <li className={interviewerClass} onClick={props.setInterviewer}>
     <img
       className="interviewers__item-image"
       src={props.avatar}
       alt={props.name}
     />
-  </li> 
-  } else {
-    return <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
-    <img
-      className="interviewers__item-image"
-      src={props.avatar}
-      alt={props.name}
-    />
-  {props.name}
-  </li> 
-
-  }
+    {props.selected && props.name}
+  </li>
+);
  
 }
 
