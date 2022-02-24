07a384ecdff9ada757e46f98623ac02f50050be0
Carter <carter.hemington@gmail.com>
2022-02-23 20:33:30 -0500

    final wed commit

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 
 export default function Appointment(props) {
-  if (!props.time) return <article className="appointment">No Appointments</article>
-  if (props.time) return <article className="appointment">Appointment at {props.time}</article>
+
+if (!props.time) return <article className="appointment" >No Appointments</article>
+  if (props.time) return <article className="appointment" >Appointment at {props.time}</article>
+
+
 
 
 }
--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
 import Form from "components/Appointment/Form";
+import { Fragment } from "react";
 import "index.scss";
 
 
@@ -156,3 +157,10 @@ storiesOf("Button", module)
         .add("Error", () => <Error message={"Could not delete appointment."} onClose={action("onClose")} />)
         .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
         .add("Edit", () => <Form student={"Carter"} interviewer={2} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+
+        .add("Appointment Empty", () => (
+          <Fragment>
+            <Appointment id={1} time="4pm" />
+            <Appointment time="5pm" />
+          </Fragment>
+        ))
