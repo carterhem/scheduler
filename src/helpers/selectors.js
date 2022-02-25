37f3565662b9f97a16f7711338a717d333cdd329
Carter <carter.hemington@gmail.com>
2022-02-24 20:23:33 -0500

    selector added

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+
+export function getAppointmentsForDay(state, day) {
+let dayAppointments = [];
+const foundDay = state.days.find(element => element.name === day);
+// console.log("day", day)
+// console.log("state", state)
+// console.log("foundDay", foundDay)
+
+if(foundDay === undefined) {
+  return dayAppointments
+} else {
+  dayAppointments = foundDay.appointments.map(element => state.appointments[element])
+
+}
+
+  return dayAppointments;
+}
